1eb1948e742d68f0d57fc1cdf3321aaa1b86be84
saumenHub <saumendra.kumar.mohapatra@gmail.com>
2021-05-16 13:50:51 +0800

    minor changes!!

--- CSharpeAccessModifiers/CSharpeAccessModifiers.cs
+++ CSharpeAccessModifiers/CSharpeAccessModifiers.cs
@@ -16,7 +16,7 @@ namespace CSharpeAccessModifiers
             
             ProtectedInternalMethod(); // Access Modifier is PROTECT INTERNAL : We have implemeted <StoreOfFunctions> and it is internal so that I can access it from here
             
-            ProtecedMethod();  // Access Modifier is PROTECT INTERNAL : We have implemeted <StoreOfFunctions> and it is accessible here and it is also accessbile in <StoreOfFunctionsToAccess> assembly to all methods even though they have NOT implemented <StoreOfFunctions>
+            ProtecedMethod();  // Access Modifier is PROTECT : We have implemeted <StoreOfFunctions> and it is accessible here and it is also accessbile in <StoreOfFunctionsToAccess> assembly to all methods even though they have NOT implemented <StoreOfFunctions>
             
             //PrivateProtecedMethod(); // Access Modifier is Private Protected : 
         }
